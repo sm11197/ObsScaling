812febdaf67e426661d1ceca49f8915562603881
sm11197 <sm11197@nyu.edu>
2024-06-26 12:43:04 -0500

    clean & re-add, FLOPs*family slopes model, final output file

--- minus_1_AND_glmm.r
+++ minus_1_AND_glmm.r
@@ -1,18 +1,27 @@
-# if (!require(pROC)) install.packages("pROC", dependencies=TRUE)
-# if (!require(caret)) install.packages("caret", dependencies=TRUE)
-# args <- c() # nolint: commented_code_linter, commented_code_linter.
 # args[1] <- "~/git/predicting-capabilities/ObsScaling/" # nolint: commented_code_linter, line_length_linter.
-main <- function() { # nolint: cyclocomp_linter.
+main <- function() { # nolint: function_name_linter
   args <- commandArgs(trailingOnly = TRUE)
-  #### Libraries, if not yet installed use install.packages("package_name")
-  library(lme4)
+  #### Libraries ####
+  ## List of required packages in alphabetical order
+  packages <- c("caret", "dplyr", "ggplot2", "gtools", "lme4", "pROC")
+  ## Function to install missing packages and load them
+  install_and_load <- function(pkg) {
+    if (!require(pkg, character.only = TRUE)) {
+      install.packages(pkg, dependencies = TRUE)
+      library(pkg, character.only = TRUE)
+    }
+  }
+  ## Apply the function to the list of packages
+  lapply(packages, install_and_load)
+  ## Load packages
+  library(caret)
   library(dplyr)
   library(ggplot2)
-  library(pROC)
-  library(caret)
   library(gtools)
+  library(lme4)
+  library(pROC)
 
-  #### DATA PREP/EXPLROATION
+  #### DATA PREP/EXPLORATION ####
   ## Read in the data
   setwd(args[1])
   base_llm_benchmark_eval <- read.csv(file.path(
@@ -47,7 +56,7 @@ main <- function() { # nolint: cyclocomp_linter.
     base_llm_emergent_eval$Model
   )
 
-  # Consolidate LLAMA and QWEN model families
+  ## Consolidate LLAMA and QWEN model families ####
   consolidate_model_family <- function(family) {
     if (grepl("OPENLLAMA", family, ignore.case = TRUE)) {
       return("OPENLLAMA")
@@ -61,8 +70,11 @@ main <- function() { # nolint: cyclocomp_linter.
       return(family)
     }
   }
+  base_llm_benchmark_eval$Model.Family <- sapply(
+    base_llm_benchmark_eval$Model.Family,
+    consolidate_model_family
+  )
 
-  base_llm_benchmark_eval$Model.Family <- sapply(base_llm_benchmark_eval$Model.Family, consolidate_model_family)
   ## Merge datasets by Model, all. Suffix "benchmark" and "emergent"
   base_llm <- merge(
     base_llm_benchmark_eval, base_llm_emergent_eval,
@@ -72,28 +84,27 @@ main <- function() { # nolint: cyclocomp_linter.
   base_llm[sapply(base_llm, is.character)] <- lapply(
     base_llm[sapply(base_llm, is.character)], as.factor
   )
-  # Count the number of models in each family
+
+  ## Count the number of models in each family ####
   family_counts <- table(base_llm$Model.Family)
-
-  # Identify families with at least 3 models
-  valid_families <- names(family_counts[family_counts >= 3])
-
-  # Filter the dataset to keep only the valid families
+  ## Identify families with at least 2 models
+  valid_families <- names(family_counts[family_counts >= 2])
+  ## Filter the dataset to keep only the valid families
   base_llm <- base_llm[base_llm$Model.Family %in% valid_families, ]
-
-  # Re-factor Model.Family to remove unused levels
+  ## Re-factor Model.Family to remove unused levels
   base_llm$Model.Family <- factor(base_llm$Model.Family)
 
   ## Summary stats
   print(summary(base_llm))
 
-  # Print the number of models remaining
+  ## Print the number of models remaining
   print(paste("Number of models remaining:", nrow(base_llm)))
 
-  # Print the remaining model families and their counts
+  ## Print the remaining model families and their counts
   remaining_family_counts <- table(base_llm$Model.Family)
   print("Remaining model families and their counts:")
   print(remaining_family_counts)
+
   ## If you want to make a plot like I shared to group + emergent benchmarks
   # install.packages("PerformanceAnalytics") # nolint: commented_code_linter.
   # library(PerformanceAnalytics) # nolint: commented_code_linter.
@@ -105,7 +116,7 @@ main <- function() { # nolint: cyclocomp_linter.
   ## You can check help for more options, but basically first argument = data
   ## (probably should manipulate this to zoom in to specific relationships)
 
-  ## Get n-1 score
+  ## Get n-1 score ####
   ## Sort the data frame by Model.Family and FLOPs
   base_llm <- base_llm %>%
     arrange(Model.Family, FLOPs..1E21.) # nolint: object_usage_linter.
@@ -114,34 +125,34 @@ main <- function() { # nolint: cyclocomp_linter.
     current_family <- data$Model.Family[model_index]
     current_flops <- data$FLOPs..1E21.[model_index]
     current_score <- data[[benchmark_column]][model_index]
-    # Filter to smaller than current model_index flops
+    ## Filter to smaller than current model_index flops
     smaller_models <- data %>%
       filter(FLOPs..1E21. < current_flops) %>% # nolint: object_usage_linter.
       arrange(desc(FLOPs..1E21.))
-    # First, try to locate the closest smaller model in the same family
+    ## First, try to locate the closest smaller model in the same family
     closest_within_family <- smaller_models %>%
       filter(Model.Family == current_family) %>% # nolint: object_usage_linter.
       slice(1)
-    # Second, if no model in family is found, use the closest global model
+    ## Second, if no model in family is found, use the closest global model
     closest_global <- smaller_models %>%
       slice(1)
-    # Determine the correct model source (within family or global)
-    # fallback to current if no smaller exists
+    ## Determine the correct model source (within family or global)
+    ## fallback to current if no smaller exists
     if (nrow(closest_within_family) > 0) {
       target_model <- closest_within_family
     } else if (nrow(closest_global) > 0) {
       target_model <- closest_global
     } else {
-      # Fallback to current model's values if no smaller models exist
+      ## Fallback to current model's values if no smaller models exist
       target_model <- tibble(
         `FLOPs..1E21.` = current_flops,
         !!benchmark_column := current_score # nolint: object_usage_linter.
       )
     }
-    # Calculate and return the normalized score directly
+    ## Calculate and return the normalized score directly
     minus_1_flops <- target_model$FLOPs..1E21.[1]
     minus_1_score <- target_model[[benchmark_column]][1]
-    # Final normalized score calculation
+    ## Final normalized score calculation
     return(minus_1_score) * (log(current_flops) / log(minus_1_flops))
   }
   ## Single column use. Apply the function to create 'GSM8K_minus_1'
@@ -149,17 +160,18 @@ main <- function() { # nolint: cyclocomp_linter.
   #  get_minus_1(idx, base_llm, "GSM8K") # nolint: commented_code_linter.
   # })
 
-  # Define non-benchmark columns
+  ## Define non-benchmark columns
   non_benchmark_columns <- c(
     "Model", "Model.Family",
     "Model.Size..B.", "Pretraining.Data.Size..T.", "FLOPs..1E21.",
     "Repo.benchmark", "Repo.emergent"
   )
-  # Identify benchmark columns by exclusion
+  ## Identify benchmark columns by exclusion
   benchmark_columns <- setdiff(names(base_llm), non_benchmark_columns)
-  # Number of trials (assuming it's a constant across benchmarks)
-  # Apply the 'get_minus_1_normalized' function across all benchmarks
+
+  ## Number of trials (assumed constant across benchmarks bc we're using acc)
   number_of_trials <- 100 # Replace as needed
+  ## Apply the 'get_minus_1_normalized' function across all benchmarks
   for (benchmark in benchmark_columns) {
     new_column_name <- paste(benchmark, "minus_1", sep = "_")
     base_llm[[new_column_name]] <- sapply(
@@ -168,22 +180,18 @@ main <- function() { # nolint: cyclocomp_linter.
         get_minus_1_normalized(idx, base_llm, benchmark)
       }
     )
-    # Dynamically create successes and failures columns as well
+    ## Dynamically create successes and failures columns as well for response
     base_llm[[paste(benchmark, "successes", sep = "_")]] <-
       round(base_llm[[benchmark]] * number_of_trials)
     base_llm[[paste(benchmark, "failures", sep = "_")]] <-
       number_of_trials - base_llm[[paste(benchmark, "successes", sep = "_")]]
   }
-  # base_llm : 107 rows, 71 cols 06/19
+  # base_llm : 107 rows, 71 cols 06/19 || 06/26 base_llm 101, 71
 
-  ### MODELING
-  ## REMOVED: Example of how to fit a single mixed effects model
-
-  # TODO: grid search of all possible predictive benchmarks. Done: 06/12 flops, n-1 # nolint
-  # Define the cutoff for FLOPs to split the data
+  ### MODELING ####
+  ## Define the cutoff for FLOPs to split the data
   cutoff_flops <- 8.4 * 10
-  # Function to fit the model with error handling
-  # Function to fit the fixed-effects model
+  ## Function to fit the fixed-effects model with error handling
   fit_model_fixed <- function(formula, data) {
     model <- tryCatch(
       {
@@ -206,6 +214,7 @@ main <- function() { # nolint: cyclocomp_linter.
     )
     return(model)
   }
+  ## Function to fit the mixed-effects model with error handling
   fit_model <- function(formula, data) {
     model <- tryCatch(
       {
@@ -229,12 +238,11 @@ main <- function() { # nolint: cyclocomp_linter.
     )
     return(model)
   }
-
+  ## Function to get model metrics with error handling
   calculate_model_metrics <- function(model, test_data, response_formula) {
     if (is.null(model) || inherits(model, "try-error") || all(is.na(model))) {
-      return(c(NA, NA, NA))
+      return(rep(NA, 3))
     }
-
     tryCatch(
       {
         if (inherits(model, "glm")) {
@@ -249,11 +257,9 @@ main <- function() { # nolint: cyclocomp_linter.
           warning("Unexpected model class")
           return(c(NA, NA, NA))
         }
-
         actual_values <- test_data[[response_formula]] / 100
         accuracy <- mean(round(actual_values, 1) == round(test_predictions, 1))
         rmse <- sqrt(mean((actual_values - test_predictions)^2))
-
         return(c(aic_value, accuracy, rmse))
       },
       error = function(e) {
@@ -263,10 +269,9 @@ main <- function() { # nolint: cyclocomp_linter.
     )
   }
 
-  # # Container for results
+  ## Container for best results
   # best_models <- list()
-
-  # Container for performance metrics
+  ## Container for performance metrics
   performance_metrics <- data.frame(
     benchmark = character(),
     model_type = character(),
@@ -276,62 +281,76 @@ main <- function() { # nolint: cyclocomp_linter.
     stringsAsFactors = FALSE
   )
 
+  ## Get all combinations of benchmarks for formulas
   benchmark_minus_1_columns <- paste0(benchmark_columns, "_minus_1")
-  all_combinations <- do.call(c, lapply(1:3, function(k) {
+  all_combinations <- do.call(c, lapply(1:2, function(k) {
     combn(benchmark_minus_1_columns, k, simplify = FALSE)
   })) # use 1:16 for all combinations except empty set
 
   for (benchmark in benchmark_columns) {
-    # Dynamic responses based on current benchmark
+    ## Dynamic responses (Y) based on current benchmark
     response_formula <- paste(benchmark, "successes", sep = "_")
     response_failures <- paste(benchmark, "failures", sep = "_")
     response <- paste("cbind(", response_formula, ",", response_failures, ")", sep = "")
     benchmark_minus_1 <- paste0(benchmark, "_minus_1")
 
-    # Remove NAs
+    ## Remove NAs # TODO:dynamic depending on formula
     base_llm_clean <- base_llm[complete.cases(base_llm[c(response_formula, response_failures, "FLOPs..1E21.", "Model.Family", benchmark_minus_1)]), ]
 
-    # Split data into training and testing sets
-    train_data <- base_llm_clean %>% filter(FLOPs..1E21. <= cutoff_flops)
-    test_data <- base_llm_clean %>% filter(FLOPs..1E21. > cutoff_flops)
+    ## Split data into training and testing sets # dynamic as well?
+    train_data <- base_llm_clean %>% filter(FLOPs..1E21. <= cutoff_flops) # nolint: object_usage_linter
+    test_data <- base_llm_clean %>% filter(FLOPs..1E21. > cutoff_flops) # nolint: object_usage_linter
 
-    # Model 0: Base model with FLOPs only (fixed-effects)
+    ## Model 0: Base model with FLOPs only (fixed-effects)
     formula_base <- as.formula(paste(response, "~ 1 + log(FLOPs..1E21.)"))
     model_base <- fit_model_fixed(formula_base, train_data)
     base_metrics <- calculate_model_metrics(model_base, test_data, response_formula)
     performance_metrics[nrow(performance_metrics) + 1, ] <- c(benchmark, "Flops Only", base_metrics[1], base_metrics[2], base_metrics[3])
 
-    # Model 1: Base model with FLOPs only + intercept
+    ## Model 1: Base model with FLOPs only + random family intercepts
     formula_base <- as.formula(paste(response, "~ log(FLOPs..1E21.) + (1|Model.Family)"))
     model_base <- fit_model(formula_base, train_data)
-    base_metrics <- calculate_model_metrics(model_base)
-    performance_metrics[nrow(performance_metrics) + 1, ] <- c(benchmark, "Flops Only", base_metrics[1], base_metrics[2], base_metrics[3])
-
     base_metrics <- calculate_model_metrics(model_base, test_data, response_formula)
     performance_metrics[nrow(performance_metrics) + 1, ] <- c(benchmark, "Flops Only + intercept", base_metrics[1], base_metrics[2], base_metrics[3])
 
-    # Model 2: Base model with FLOPs and benchmark minus 1
+    ## Model 1.5: Base model with FLOPs only + random family intercepts & slopes
+    formula_base <- as.formula(paste(response, "~ log(FLOPs..1E21.) + (1 + log(FLOPs..1E21.)|Model.Family)"))
+    model_base <- fit_model(formula_base, train_data)
+    base_metrics <- calculate_model_metrics(model_base, test_data, response_formula)
+    performance_metrics[nrow(performance_metrics) + 1, ] <- c(benchmark, "Flops Only + intercept-slope", base_metrics[1], base_metrics[2], base_metrics[3])
+
+    ## Model 2: Base model with FLOPs, random family intercept, benchmark_minus_1
     formula_minus_1 <- as.formula(paste0(response, " ~ log(FLOPs..1E21.) + (1|Model.Family) + ", benchmark_minus_1))
     model_minus_1 <- fit_model(formula_minus_1, train_data)
     minus_1_metrics <- calculate_model_metrics(model_minus_1, test_data, response_formula)
-    performance_metrics[nrow(performance_metrics) + 1, ] <- c(benchmark, "Flops + n-1", minus_1_metrics[1], minus_1_metrics[2], minus_1_metrics[3])
+    performance_metrics[nrow(performance_metrics) + 1, ] <- c(benchmark, "Flops + intercept + n-1", minus_1_metrics[1], minus_1_metrics[2], minus_1_metrics[3])
 
-    # Grid search: Add each of the other benchmarks minus 1 incrementally
-    other_benchmarks <- setdiff(benchmark_columns, benchmark)
+    ## TODO: generalize the above?
+
+    ## Grid search #### 06/26, FLOPs, family intercepts,
     formula_components <- "log(FLOPs..1E21.) + (1|Model.Family)"
-
-    for (other_benchmark in other_benchmarks) {
-      other_benchmark_minus_1 <- paste0(other_benchmark, "_minus_1")
-      if (other_benchmark_minus_1 %in% colnames(base_llm_clean)) {
-        formula_components <- paste0(formula_components, " + ", other_benchmark_minus_1)
-        formula_dynamic <- as.formula(paste0(response, " ~ ", formula_components))
-        model_dynamic <- fit_model(formula_dynamic, train_data)
-        dynamic_metrics <- calculate_model_metrics(model_dynamic)
-        performance_metrics[nrow(performance_metrics) + 1, ] <- c(benchmark, paste("Flops +", other_benchmark_minus_1), dynamic_metrics[1], dynamic_metrics[2], dynamic_metrics[3])
+    formulas <- lapply(seq_along(all_combinations), function(comb) {
+      components <- paste(all_combinations[comb], collapse = " + ")
+      formula_str <- paste(response, "~", formula_components, "+", components)
+      as.formula(formula_str)
+    })
+    for (formula in formulas) {
+      model_dynamic <- fit_model(formula, train_data)
+      dynamic_metrics <- calculate_model_metrics(model_dynamic, test_data, response_formula)
+      if (!is.na(dynamic_metrics[1])) {
+        performance_metrics[nrow(performance_metrics) + 1, ] <- c(
+          benchmark,
+          paste(formula[3]),
+          dynamic_metrics[1],
+          dynamic_metrics[2],
+          dynamic_metrics[3]
+        )
       }
     }
 
-    # Determine the best model for this benchmark
+    ## Determine the best model for this benchmark
+    ## commented out bc we're retaining all results now,
+    ## performance metrics out of loop
     # if (any(!is.na(performance_metrics$rmse))) {
     #   best_model <- performance_metrics %>%
     #     filter(
@@ -350,7 +369,7 @@ main <- function() { # nolint: cyclocomp_linter.
   # Optionally, plot ROC curve for the last evaluated benchmark
   # plot(roc_obj)
 
-  # Done? train/test split
+  write.csv(performance_metrics, file.path(getwd(), "performance_metrics.csv"))
 }
 
 main()
